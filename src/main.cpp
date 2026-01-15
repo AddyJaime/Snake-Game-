b51a9a3d0e6df7000f95787aac37cfe6dda53b3c
addy <addyjaime@gmail.com>
2026-01-15 18:53:36 -0500

    making the game stop and reset

--- src/main.cpp
+++ src/main.cpp
@@ -38,9 +38,21 @@ class Snake{
     public:
  // deque (double-ended queue): es una estructura de datos que permite insertar y eliminar elementos por delante y por detrás.
    deque<Vector2> body = {Vector2{10,10}, Vector2{9,10}, Vector2{8,10}};
+
+//    posicion inicial mueve hacia la derecha
+// {1,0}derecha
+// {-1,0}izquierda
+// {0,-1}arriba
+// {0, 1}abajo
+
    Vector2 direccion = {1,0};
+    // 🔴 ESTA VARIABLE NO AGREGA NADA POR SÍ SOLA
+    // Es SOLO UNA SEÑAL que dice:
+    // “En el PRÓXIMO movimiento, el snake debe crecer”
+   bool addSegment = false;
 
     void Draw()
+
     {
         for(int i = 0; i < body.size(); i++){
             float x = body[i].x ;
@@ -53,13 +65,37 @@ class Snake{
     // aqui se va a lograr el movimiento de la serpiente, quitando y metiendo 
     void Update()
     {
-        body.pop_back();
         // direccion Es hacia dónde te mueves
-            // body[0] Es la posición ACTUAL de la cabeza
-            // Vector2Add(a, b) Suma dos posiciones
-            // El snake se mueve quitando la cola y agregando una nueva cabeza adelante.
-            // this will move the snake's head in the direcion specified by the direction attribute
-            body.push_front(Vector2Add(body[0], direccion));
+        // body[0] Es la posición ACTUAL de la cabeza
+        // Vector2Add(a, b) Suma dos posiciones
+        // El snake se mueve quitando la cola y agregando una nueva cabeza adelante.
+        // this will move the snake's head in the direcion specified by the direction attribute
+        // esta linea estaba dentro del if y el else pero la saque y ahora esta afuera
+        // 👉 Si una línea se ejecuta SIEMPRE, no debe estar dentro de un if/else.
+        // el snake siempre se mueve osea siempre hay que agregar una cabeza se mueva o no 
+        body.push_front(Vector2Add(body[0], direccion));
+
+
+            // 👉 Si addSegment es true:
+            // NO quitamos la cola
+            // Solo agregamos una nueva cabeza
+            // Resultado: el snake CRECE en 1 segment
+        if(addSegment){
+            
+            // Ya cumplimos la orden de crecer,
+            // así que volvemos la señal a false
+            addSegment = false;
+        } else
+        {
+            body.pop_back();
+          
+        }
+    }
+
+    void Reset()
+    {
+        body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+        direccion = {1,0};
     }
 
 };
@@ -104,6 +140,9 @@ class Game {
     public:
         Food comida = Food();
         Snake snake = Snake();
+        bool running = true;
+        
+
 
         void Draw()
         {
@@ -113,9 +152,50 @@ class Game {
 
         void Update()
         {
-            snake.Update();
+            if (running)
+            {
+                snake.Update();
+                CheckCollisionWithFood();
+                CheckCollisionWithEdge();   
+            }
         }
 
+        void CheckCollisionWithFood()
+        {
+            if(Vector2Equals(snake.body[0], comida.position)){
+                
+                comida.position = comida.GenerateRandonPos();
+                // En el próximo movimiento, crecer, es solo una nota
+                // aqui decetamos la colision para poder agregar un segment 
+                snake.addSegment = true;
+                
+            }
+        }
+        
+        void CheckCollisionWithEdge()
+        {
+          
+            if (snake.body[0].x == CANTIDAD_CELDAS || snake.body[0].x == -1)
+            {
+             
+                GameOver();
+               
+            }
+
+            if (snake.body[0].y == CANTIDAD_CELDAS || snake.body[0].y == -1)
+            {
+                GameOver();
+                          
+            }
+            
+        }
+
+        void GameOver()
+        {
+           snake.Reset();
+           running = false;
+         
+        }
 
 };
 
@@ -165,14 +245,7 @@ int main()
         }
         
 
-            // 3️⃣ COMER COMIDA
-            //     if (snake.body[0].x == comida.position.x && snake.body[0].y == comida.position.y)
-            //     {
-            //         comida.position.x = GetRandomValue(0, CANTIDAD_CELDAS - 1);
-            //         comida.position.y = GetRandomValue(0, CANTIDAD_CELDAS - 1);
-
-                
-            //     }
+    
                     
             // // 4️⃣ LÍMITES DEL MAPA
             // if (snake.body[0].x < 0 || snake.body[0].x >= CANTIDAD_CELDAS ||
@@ -189,6 +262,7 @@ int main()
 
         if(eventTriggered(0.3)){
            game.snake.Update();
+           game.CheckCollisionWithFood();
         }
 
 
